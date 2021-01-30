3ed75752bb7b74faeab40190adcb65c8f682634f
carneiroArleilson <carneiroarleilson@gmail.com>
2021-01-30 16:49:39 -0400

    feat: create get project

--- src/routes/project.route.ts
+++ src/routes/project.route.ts
@@ -1,5 +1,28 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import { ProjectORM } from '../entities/project.entity';
 
 const projectRoute = Router();
+
+projectRoute.get('/', async (request, response) => {
+  try {
+    const take = Number((request.query.take) || 5);
+    const skip = Number((request.query.skip) || 0);
+
+    const projectRepository = getRepository(ProjectORM);
+
+    const projects = await projectRepository.find({ take, skip });
+
+    return response.json(projects);
+  } catch (error) {
+    return response.status(404).json({ message: error });
+  }
+});
+
+projectRoute.post('/', (request, response) => {});
+
+projectRoute.put('/:id_project', (request, response) => {});
+
+projectRoute.delete('/:id_project', (request, response) => {});
 
 export default projectRoute;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,4 +2,7 @@ import { Router } from 'express';
 
 const userRoute = Router();
 
+userRoute.get('/', (request, response) => {});
+userRoute.post('/', (request, response) => {});
+
 export default userRoute;
