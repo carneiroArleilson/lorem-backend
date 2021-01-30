7e7a14e9039a0c8c11e10fe7bbfef25409261ce0
carneiroArleilson <carneiroarleilson@gmail.com>
2021-01-30 17:55:03 -0400

    feat: create GET user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,31 @@
 import { Router } from 'express';
+import { getRepository, Raw } from 'typeorm';
+import { UserORM } from '../entities/user.entity';
 
 const userRoute = Router();
 
-userRoute.get('/', (request, response) => {});
+userRoute.get('/', async (request, response) => {
+  try {
+    const take = Number(request.query.take || 5);
+    const skip = Number(request.query.skip || 0);
+    const name = request.query.name;
+
+    const userRepository = getRepository(UserORM);
+
+    const users = await userRepository.find({
+      where: {
+        name: Raw(alias => (name ? `alias ILIKE '%${name}%'` : 'true')),
+      },
+      take,
+      skip,
+    });
+
+    return response.json(users);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
 userRoute.post('/', (request, response) => {});
 
 export default userRoute;
