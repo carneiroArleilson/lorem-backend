0f18c7ceb1ca82f79ca539d36777b7b3f20da4dd
carneiroArleilson <carneiroarleilson@gmail.com>
2021-01-30 16:17:43 -0400

    fix: update tsconfig and create router

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import router from './routes';
 
 import './database';
 
@@ -6,6 +7,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.use('/api');
+app.use('/api', router);
 
 export default app;
